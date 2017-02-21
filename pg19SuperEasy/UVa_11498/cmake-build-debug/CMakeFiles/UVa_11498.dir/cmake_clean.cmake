bd837311333c81dca53ad013fe6f76846297f7c6
kaero <kevinwkt97@outlook.com>
2017-02-21 02:18:09 -0600

    SuperEasy

---
+++ pg19SuperEasy/UVa_11498/cmake-build-debug/CMakeFiles/UVa_11498.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa_11498.dir/main.cpp.obj"
+  "UVa_11498.pdb"
+  "UVa_11498.exe"
+  "UVa_11498.exe.manifest"
+  "libUVa_11498.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa_11498.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
