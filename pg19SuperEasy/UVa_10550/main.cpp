5d6f8f9be482e867aaa6dff9560cd172d13ed158
kaero <kevinwkt97@outlook.com>
2017-02-21 01:29:57 -0600

    SuperEasy

--- pg19SuperEasy/UVa_10550/main.cpp
+++ pg19SuperEasy/UVa_10550/main.cpp
@@ -6,8 +6,8 @@ int main() {
     int a,b,c,d,tot;
 
 
-    while(scanf("%d %d %d %d",&a,&b,&c,&d)&&(a||b||c||d)){
-        tot=1080 + ((a - b + 40) % 40 + (c - b + 40) % 40 + (c - d + 40) % 40) * 9;
-        printf("%d\n",tot);
+    while(scanf("%d %d %d %d",&a,&b,&c,&d)&&(a||b||c||d)) {
+        tot = 1080 + ((a - b + 40) % 40 + (c - b + 40) % 40 + (c - d + 40) % 40) * 9;
+        printf("%d\n", tot);
     }
 }
