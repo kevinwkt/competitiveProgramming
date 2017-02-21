f5fe27c970bab7f6e2041b7a877698e804e12559
kaero <kevinwkt97@outlook.com>
2017-02-21 17:53:42 -0600

    SuperEasy

--- pg19SuperEasy/UVa_12577/main.cpp
+++ pg19SuperEasy/UVa_12577/main.cpp
@@ -8,12 +8,12 @@ int main() {
     int c=1;
     string s;
 
-    while(1){
-        cin>>s;
-        if(s=="*") return 0;
+    while(1) {
+        cin >> s;
+        if (s == "*") return 0;
 
-        if(s=="Hajj") printf("Case %d: Hajj-e-Akbar\n",c);
-        if(s=="Umrah") printf("Case %d: Hajj-e-Asghar\n",c);
+        if (s == "Hajj") printf("Case %d: Hajj-e-Akbar\n", c);
+        if (s == "Umrah") printf("Case %d: Hajj-e-Asghar\n", c);
         c++;
 
     }
