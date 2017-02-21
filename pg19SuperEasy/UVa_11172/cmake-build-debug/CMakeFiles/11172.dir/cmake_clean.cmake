68059df0a6a3b6647ea3df1822507c9dcf27d7c4
kaero <kevinwkt97@outlook.com>
2017-02-21 00:31:28 -0600

    SuperEasy

---
+++ pg19SuperEasy/UVa_11172/cmake-build-debug/CMakeFiles/11172.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11172.dir/main.cpp.obj"
+  "11172.pdb"
+  "11172.exe"
+  "11172.exe.manifest"
+  "lib11172.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11172.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pg19SuperEasy/UVa_11172/cmake-build-debug/CMakeFiles/11172.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 11172.
+# This may be replaced when dependencies are built.
