d95c3c6f45cd633eeacd8f316b9cca08de0fcb9d
kaero <kevinwkt97@outlook.com>
2017-02-21 00:11:40 -0600

    10550

---
+++ pg19SuperEasy/UVa_10550/cmake-build-debug/CMakeFiles/UVa_10550.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa_10550.dir/main.cpp.obj"
+  "UVa_10550.pdb"
+  "UVa_10550.exe"
+  "UVa_10550.exe.manifest"
+  "libUVa_10550.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa_10550.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
