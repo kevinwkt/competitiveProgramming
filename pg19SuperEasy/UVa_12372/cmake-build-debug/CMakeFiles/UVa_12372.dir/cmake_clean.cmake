9fb3e87239b04695745aae0cbb2b6d3fd8457b91
kaero <kevinwkt97@outlook.com>
2017-02-21 16:08:15 -0600

    SuperEasy

---
+++ pg19SuperEasy/UVa_12372/cmake-build-debug/CMakeFiles/UVa_12372.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa_12372.dir/main.cpp.obj"
+  "UVa_12372.pdb"
+  "UVa_12372.exe"
+  "UVa_12372.exe.manifest"
+  "libUVa_12372.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa_12372.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pg19SuperEasy/UVa_12372/cmake-build-debug/CMakeFiles/UVa_12372.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UVa_12372.
+# This may be replaced when dependencies are built.
