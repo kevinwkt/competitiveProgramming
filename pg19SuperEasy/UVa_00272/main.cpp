fec5e3fa18751f03c0971db86f6898622a67deed
kaero <kevinwkt97@outlook.com>
2017-02-21 01:30:55 -0600

    SuperEasy

--- pg19SuperEasy/UVa_00272/main.cpp
+++ pg19SuperEasy/UVa_00272/main.cpp
@@ -7,15 +7,15 @@ int main() {
     char c;
     bool cond=true;
 
-    while(scanf("%c",&c)!=EOF){
-        if(c==34&&cond){
+    while(scanf("%c",&c)!=EOF) {
+        if (c == 34 && cond) {
             printf("``");
-            cond=false;
-        }else if(c==34&&cond!=true){
+            cond = false;
+        } else if (c == 34 && cond != true) {
             printf("''");
-            cond=true;
-        }else{
-            printf("%c",c);
+            cond = true;
+        } else {
+            printf("%c", c);
         }
     }
 }
