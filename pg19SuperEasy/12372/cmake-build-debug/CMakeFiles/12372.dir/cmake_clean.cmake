4544e41770604c98013577b7198a072e306497c2
kaero <kevinwkt97@outlook.com>
2017-02-21 15:52:00 -0600

    SuperEasy

---
+++ pg19SuperEasy/12372/cmake-build-debug/CMakeFiles/12372.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/12372.dir/main.cpp.obj"
+  "12372.pdb"
+  "12372.exe"
+  "12372.exe.manifest"
+  "lib12372.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12372.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
