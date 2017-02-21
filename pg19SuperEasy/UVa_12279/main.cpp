070e463a7e570fc4551a9275e940ee731c609c5b
kaero <kevinwkt97@outlook.com>
2017-02-21 12:34:02 -0600

    SuperEasy

---
+++ pg19SuperEasy/UVa_12279/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int a,d;
+    d=1;
+    while(1){
+        scanf("%d",&a);
+        if(a==0) return 0;
+        int x,t,c;
+        c=0;
+        t=0;
+        for(int i=0;i<a;i++){
+
+            scanf("%d",&x);
+            if(x>0 && x<100) c++;
+            if(x==0) t++;
+        }
+        c=c-t;
+        printf("Case %d: %d\n",d,c);
+        d++;
+    }
+}
