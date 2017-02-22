751a60ebe08f98e7b3496699da5d5449836c6dd3
kaero <kevinwkt97@outlook.com>
2017-02-22 01:43:19 -0600

    Easy

---
+++ pg20Easy/UVa_10114/cmake-build-debug/CMakeFiles/UVa_10114.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa_10114.dir/main.cpp.obj"
+  "UVa_10114.pdb"
+  "UVa_10114.exe"
+  "UVa_10114.exe.manifest"
+  "libUVa_10114.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa_10114.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pg20Easy/UVa_10300/cmake-build-debug/CMakeFiles/UVa_10300.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa_10300.dir/main.cpp.obj"
+  "UVa_10300.pdb"
+  "UVa_10300.exe"
+  "UVa_10300.exe.manifest"
+  "libUVa_10300.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa_10300.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
