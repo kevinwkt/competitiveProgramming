94ba7a90bd292c6892df8358aa6ee1267c3b3afe
kaero <kevinwkt97@outlook.com>
2017-02-22 01:58:27 -0600

    Easy

--- pg20Easy/UVa_10300/main.cpp
+++ pg20Easy/UVa_10300/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <stdio.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a,b,t1,t0,t2;
+    scanf("%d",&a);
+    while(a--){
+        int tot=0;
+        scanf("%d",&b);
+        while(b--){
+            scanf("%d %d %d",&t1,&t0,&t2);
+            tot=tot+(t1*t2);
+        }
+        printf("%d\n",tot);
+    }
 }
---
+++ pg20Easy/UVa_10963/cmake-build-debug/CMakeFiles/UVa_10963.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UVa_10963.
+# This may be replaced when dependencies are built.
