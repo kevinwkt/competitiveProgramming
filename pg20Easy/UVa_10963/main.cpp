ced65aeccf92c5b433c1df9a9d2942340698c8e2
kaero <kevinwkt97@outlook.com>
2017-02-22 18:26:27 -0600

    Easy

--- pg20Easy/UVa_10963/main.cpp
+++ pg20Easy/UVa_10963/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <stdio.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a,b,c,t1,t2;
+    bool co;
+
+    scanf("%d",&a);
+
+    while(a--){
+        co=true;
+        scanf("%d",&b);
+        scanf("%d %d",&t1,&t2);
+        c=t1-t2;
+        b--;
+        while(b--){
+            scanf("%d %d",&t1,&t2);
+            if(c!=(t1-t2)) {
+                co=false;
+            }
+        }
+
+        if(co){
+            printf("yes\n");
+        }else printf("no\n");
+
+        if(a!=0){
+            printf("\n");
+        }
+
+    }
 }
