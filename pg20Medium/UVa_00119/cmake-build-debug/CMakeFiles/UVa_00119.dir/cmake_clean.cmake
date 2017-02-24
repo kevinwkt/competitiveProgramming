986c0ce2be28220f1c5958cb8e25f285b08e6839
kaero <kevinwkt97@outlook.com>
2017-02-23 18:33:03 -0600

    Easy

---
+++ pg20Medium/UVa_00119/cmake-build-debug/CMakeFiles/UVa_00119.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVa_00119.dir/main.cpp.obj"
+  "UVa_00119.pdb"
+  "UVa_00119.exe"
+  "UVa_00119.exe.manifest"
+  "libUVa_00119.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVa_00119.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
